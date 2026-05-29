aa7f73817c85e2b7f68d2902b022dab3b887b99e
Eloisy Moreira <eloisy06@gmail.com>
2026-05-29 09:14:38 -0300

    cria EventController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,28 +6,11 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-
-    public function index()
-    {
-        $nome = "Eloisy";
-        $idade = 24;
-
-        $arr = [1, 2, 3, 4, 5];
-
-        $nomes = ["Eliana", "Edson", "Euripia", "Alberto", "Marcos"];
-
-        return view(
-            'welcome',
-            [
-                'nome' => $nome,
-                'idade2' => $idade,
-                'profissao' => "Analista de dados",
-                'arr' => $arr,
-                'nomes' => $nomes
-            ]);
+    public function index() {
+        return view('welcome');
     }
 
-    public function create(){
+    public function create() {
         return view('events.create');
     }
 }
